b07761b9028ce8547fd32aa0f2c5f1c9d4f637bc
Ashik Pirmohamed <ashik1000@hotmail.com>
2017-10-24 22:49:21 +0100

    added class for get_iplayer

---
+++ pyiplayer.py
@@ -0,0 +1,33 @@
+import subprocess
+
+
+class iplayer(object):
+    def search(self, query):
+        p = subprocess.Popen(["get_iplayer", query],
+                             stdout=subprocess.PIPE)
+        output, err = p.communicate()
+
+        try:
+            print err
+        except:
+            print output
+
+        print ("done")
+        print (output)
+
+    def list(self):
+        p = subprocess.Popen(["transmission-remote", "-n", "transmission:transmission", "-l"], stdout=subprocess.PIPE)
+        output, err = p.communicate()
+
+        try:
+            print err
+        except:
+            print output
+
+        print ("done")
+        print (output)
+
+
+status = iplayer()
+
+status.list('Strictly')
