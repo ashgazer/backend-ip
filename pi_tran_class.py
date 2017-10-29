5a210041b135a98e4576aaabab56a405696fcf6e
Ashik <ashik1000@hotmail.com>
2017-10-29 19:59:40 +0000

    added trans mission class

---
+++ pi_tran_class.py
@@ -0,0 +1,39 @@
+import subprocess
+
+
+class transmission(object):
+
+	def add_torrent(self,magnet):
+		p = subprocess.Popen(["transmission-remote","-n", "transmission:transmission" ,"-a",magnet], stdout=subprocess.PIPE)
+		output, err = p.communicate()
+		
+
+		try:
+		        print err
+		except:
+		        print output
+
+
+		print ("done")
+		print (output)
+
+	def list(self):
+		p = subprocess.Popen(["transmission-remote","-n", "transmission:transmission" ,"-l"], stdout=subprocess.PIPE)
+		output, err = p.communicate()
+		
+
+		try:
+		        print err
+		except:
+		        print output
+
+
+		print ("done")
+		print (output)
+
+
+
+status = transmission()
+
+status.list()
+
