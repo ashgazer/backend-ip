d03f734300be51a68976d9833b0bb1d06eee0cb5
Ashik Pirmohamed <ashik1000@hotmail.com>
2017-10-29 19:13:31 +0000

    added new functionality to search through webpage

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, Markup
 
+from pybay import get_s
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,23 @@ app = Flask(__name__)
 def index():
 	
 	return ("<h1> boom </h1>")
+
+
+
+@app.route("/Search")
+def search():
+	return render_template('posting.html')
+
+
+@app.route("/find",methods=['POST','GET'])
+def find():
+	qs =  request.form["SQUERY"]
+	man = get_s(url)
+
+	site_data = man.search(qs)
+
+
+	return(site_data)
 
 
 if __name__ == "__main__":
